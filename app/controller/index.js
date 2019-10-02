c2fe38e4627d93de74e501082e3c49d960813a13
Marcos Schneider <marcos.schneider13@hotmail.com>
2019-10-02 15:06:05 -0300

    alteracoes menu historico

--- app/controller/index.js
+++ app/controller/index.js
@@ -10,13 +10,29 @@ var app = express();
 app.use(require('express-is-ajax-request'));
 
 /* GET pagina de login. */
+
 router.get('/', function(req, res, next) {
 	model.GetPrimeiroAporte(req.session.usuario.id).then(data_primeiro_aporte=>{
 		data.primeiro_aporte = data_primeiro_aporte;
-		console.log('kokokokokokoko usuario requisição kokokokokokokokoko')
-		console.log(data);
-		console.log('kokokokokokokokokokokokokokokokokokokokokokokokokoko');
-		res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'historico/historico', data: data, usuario: req.session.usuario});
+		model.GetValorSaldoAtualizado(req.session.usuario.id).then(data_saldo_atualizado =>{
+			data.saldo_atualizado = data_saldo_atualizado;
+			model.GetPlanoUsuario(req.session.usuario.id).then(data_plano=>{
+				data.plano = data_plano;
+				model.GetMesesDecorridosUsuario(req.session.usuario.id).then(data_meses_decorridos=>{
+					data.meses_decorridos = data_meses_decorridos;
+					model.GetCaixaMesesUsuario(req.session.usuario.id).then(data_meses_caixa=>{
+						data.meses_com_caixa = data_meses_caixa;
+						model.GetMesAtualAtivo().then(data_mes_atual_ativo=>{
+							data.mes_atual_ativo = data_mes_atual_ativo;
+							console.log('kokokokokokoko usuario requisição kokokokokokokokoko')
+							console.log(data);
+							console.log('kokokokokokokokokokokokokokokokokokokokokokokokokoko');
+							res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'historico/historico', data: data, usuario: req.session.usuario});
+						});
+					});
+				});
+			});
+		});
 	});
 });
 
--- app/model/indexModel.js
+++ app/model/indexModel.js
@@ -19,22 +19,99 @@ class IndexModel {
 			});
 	}
 
-
-
-	Login(POST) {
+	GetExtrato(id_usuario,dolar) {
 		return new Promise(function(resolve, reject) {
-			// Adicione a query com scape(?) e os respectivos valores em um array simples
-			helper.Query('SELECT id FROM usuarios WHERE login = ? AND senha = ?', [POST.login, POST.senha]).then(data => {
+			helper.Query('SELECT a.*,REPLACE(ROUND(a.valor,2),".",",") as valor,REPLACE(ROUND(a.qtd_stable,2),".",",") as qtd_stable,REPLACE(ROUND(a.qtd_stable * ?,2),".",",") as qtd_real_convertido,\
+				DATE_FORMAT(a.data_cadastro, "%d/%m/%Y") as data_cadastro,DATE_FORMAT(a.dia_convertido_stable, "%d/%m/%Y") as dia_convertido_stable,b.nome as nome_plano,\
+				CASE \
+				WHEN a.tipo = 0 THEN "Depósito em Conta"\
+				WHEN a.tipo = 1 THEN "Saque"\
+				WHEN a.tipo = 2 THEN "Rendimento"\
+				WHEN a.tipo = 3 THEN "Saque"\
+				ELSE 0 END as mensagem \
+				FROM caixa as a \
+				LEFT JOIN planos as b ON a.id_plano = b.id\
+				WHERE a.deletado = ? AND a.id_usuario = ? \
+				ORDER BY a.data_cadastro', [dolar,0,id_usuario]).then(data => {
+					resolve(data);
+				});
 			});
-		});
 	}
-	Teste() {
+
+	GetValorSaldoAtualizado(id_usuario) {
 		return new Promise(function(resolve, reject) {
-			// Adicione a query com scape(?) e os respectivos valores em um array simples
-			helper.Query('SELECT * FROM usuarios', []).then(data => {
-				resolve(data);
+			helper.Query('SELECT COALESCE(\
+				REPLACE(REPLACE(REPLACE(FORMAT((\
+				(SUM(CASE WHEN (tipo = ? AND deletado = ? AND id_usuario = ? AND confirmado = ?) THEN valor ELSE 0 END)) + \
+				(SUM(CASE WHEN (tipo = ? AND deletado = ? AND id_usuario = ? AND confirmado = ?) THEN valor ELSE 0 END)) - \
+				(SUM(CASE WHEN (tipo = ? AND deletado = ? AND id_usuario = ? AND confirmado = ?) THEN valor ELSE 0 END)) - \
+				(SUM(CASE WHEN (tipo = ? AND deletado = ? AND id_usuario = ? AND confirmado = ?) THEN valor ELSE 0 END)) \
+				),2), ".", "@"), ",", "."), "@", ","),0) as saldo_atualizado \
+				FROM caixa', 
+				[
+				0,0,id_usuario,1,
+				2,0,id_usuario,1,
+				1,0,id_usuario,1,
+				3,0,id_usuario,1]).then(data => {
+					resolve(data);
+				});
 			});
-		});
+	}
+
+
+	GetMesesDecorridosUsuario(id_usuario){
+		return new Promise(function(resolve, reject) {
+			helper.Query('SELECT ROUND(DATEDIFF(NOW(),data_inicio)/30) as meses_decorridos FROM usuarios_planos \
+				WHERE id_usuario = ? and deletado = ?', [id_usuario,0]).then(data => {
+					resolve(data);
+				});
+			});
+	}
+
+
+	GetPlanoUsuario(id_usuario) {
+		return new Promise(function(resolve, reject) {
+			helper.Query('SELECT b.nome, c.nome as plano, c.performance\
+				FROM usuarios_planos as a\
+				LEFT JOIN usuarios as b ON b.id = a.id_usuario\
+				LEFT JOIN planos as c ON c.id = a.id_plano\
+				WHERE a.id_usuario = ? AND a.deletado = ?', [id_usuario,0]).then(data => {
+					resolve(data);
+				});
+			});
+	}
+
+	GetMesAtualAtivo(){
+		return new Promise(function(resolve, reject) {
+			helper.Query('SELECT MONTH(NOW()) as mes_atual_ativo', []).then(data => {
+					resolve(data);
+				});
+			});
+	}
+
+
+	GetCaixaMesesUsuario(id_usuario) {
+		return new Promise(function(resolve, reject) {
+			helper.Query('SELECT MONTH(data) AS numero_mes,\
+				CASE \
+				WHEN MONTH(data) = 1 THEN "Jan"\
+				WHEN MONTH(data) = 2 THEN "Fev"\
+				WHEN MONTH(data) = 3 THEN "Mar"\
+				WHEN MONTH(data) = 4 THEN "Abr"\
+				WHEN MONTH(data) = 5 THEN "Mai"\
+				WHEN MONTH(data) = 6 THEN "Jun"\
+				WHEN MONTH(data) = 7 THEN "Jul"\
+				WHEN MONTH(data) = 8 THEN "Ago"\
+				WHEN MONTH(data) = 9 THEN "Set"\
+				WHEN MONTH(data) = 10 THEN "Out"\
+				WHEN MONTH(data) = 11 THEN "Nov"\
+				WHEN MONTH(data) = 12 THEN "Dez"\
+				END AS nome_mes\
+				FROM caixa WHERE deletado = ? AND id_usuario = ? \
+				GROUP BY MONTH(data)',[0,id_usuario]).then(data => {
+					resolve(data);
+				});
+			});
 	}
 }
 module.exports = IndexModel;
